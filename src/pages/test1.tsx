0a53c5986d0370d529ab60a06923ca81a7d159cc
Jenny Hovland <jenny.hovland@hva.nl>
2025-01-14 14:36:27 +0100

    Added fonts for first page, favicon, changed css styling for first page

--- src/pages/test1.tsx
+++ src/pages/test1.tsx
@@ -2,11 +2,22 @@ import "../styles/fontPairing1.css";
 
 const FontPairing1 = () => {
   return (
-    <div className="font-container">
-      <h1>Test1</h1>
-      <p>
-        Watch out, you might burn yourself when looking through this collection, it's hella hot.
-      </p>
+    <div className="container">
+      <div className="title">
+        <span className="title-long">
+          DEN SOM ER MED PAA LEKEN, MAA TAALE
+        </span>
+        <br></br>
+        <span className="highlight">
+          STEKEN
+        </span>
+      </div>
+      
+      <div className="paragraph">
+        <p>
+          BLABLABLA BLA BLA BLA
+        </p>
+      </div>
     </div>
   );
 };
